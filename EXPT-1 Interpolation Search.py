4c263f8d8161fcbe8227b48c8f9143d1f2fea835
jeevanrampbi <jeevanrampbi10@gmail.com>
2026-06-24 10:40:20 +0530

    Add files via upload

---
+++ EXPT-1 Interpolation Search.py
@@ -0,0 +1,22 @@
+from math import floor
+
+
+def interpolation_search(arr,target):
+    low=0
+    high=len(arr)-1
+    while low<=high and target>=arr[low] and target<=arr[high]:
+        if low==high:
+            if arr[low]==target:
+                return low
+            else:
+                return -1
+        pos=(low+(target-arr[low])*(high-low))/(arr[high]-arr[low])
+        pos=floor(pos)
+        if arr[pos]==target:
+            return pos
+        elif arr[pos]<target:
+            low=pos+1
+        else:
+            high=pos-1
+    return -1
+print(interpolation_search([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],14))
